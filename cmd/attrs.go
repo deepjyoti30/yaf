6bdc71290be37df737e81f7135e31c5569d215ed
Deepjyoti Barman <deep.barman30@gmail.com>
2022-01-18 20:36:53 +0530

    Make the util methods exported for external use

--- cmd/attrs.go
+++ cmd/attrs.go
@@ -1,4 +1,4 @@
-package piratefetch
+package cmd
 
 import (
 	"fmt"
--- main.go
+++ main.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/deepjyoti30/piratefetch/cmd"
+)
+
+func main() {
+	fmt.Println("", cmd.GetUser())
+}
