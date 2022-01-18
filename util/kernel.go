eeaf67e6b701b8c911fe89aee744e4ad32d0874e
Deepjyoti Barman <deep.barman30@gmail.com>
2022-01-18 22:33:15 +0530

    Add code to fetch kernel version

---
+++ util/kernel.go
@@ -0,0 +1,28 @@
+package util
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+)
+
+// Extract the kernel version by reading the /proc/version file
+func GetKernalVersion() string {
+	var procFile = "/proc/version"
+
+	content, err := os.ReadFile(procFile)
+	if err != nil {
+		fmt.Println("couldn't read kernel version")
+		return "not found"
+	}
+
+	return parseProc(string(content))
+}
+
+func parseProc(content string) string {
+	re := regexp.MustCompile(`version\s(.*?)\s`)
+	cleanRe := regexp.MustCompile(`version|\s`)
+
+	versionExtracted := re.FindAllString(content, 1)
+	return cleanRe.ReplaceAllString(versionExtracted[0], "")
+}
