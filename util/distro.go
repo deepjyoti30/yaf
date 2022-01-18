33cb3b3cd3d533be0f842663cdbc2b4af1a3d5ad
Deepjyoti Barman <deep.barman30@gmail.com>
2022-01-18 22:08:41 +0530

    Add function to extract distro name from os release file

--- util/distro.go
+++ util/distro.go
@@ -11,12 +11,18 @@ import (
 // Use the GOOS var as a fallback
 func GetDistroName() string {
 	var lsbFile = "/etc/lsb-release"
-	//var osReleaseFile = "/etc/os-release"
+	var osReleaseFile = "/etc/os-release"
 
 	// Try the lsb file
 	content, err := os.ReadFile(lsbFile)
 	if err == nil {
 		return parseLsbContent(string(content))
+	}
+
+	// Try the Os release file
+	content, err = os.ReadFile(osReleaseFile)
+	if err == nil {
+		return parseOsReleaseContent(string(content))
 	}
 
 	return ""
@@ -25,12 +31,36 @@ func GetDistroName() string {
 // Parse the LSB content, use regex to find the distro name
 // and return it accordingly.
 func parseLsbContent(content string) string {
-	// Replace all newlines with nothing
-	unwantedChar := regexp.MustCompile("\n|\"")
-	content = unwantedChar.ReplaceAllString(content, "")
+	// Replace all newlines and " with nothing
+	content = replaceSpecialChars(content)
 
 	re := regexp.MustCompile(".*?DISTRIB_DESCRIPTION=")
 	cleanedStr := re.ReplaceAllString(content, "")
 
 	return cleanedStr
+}
+
+// Parse the OS Release content, use regex to find the distro name
+// and return it accordingly.
+func parseOsReleaseContent(content string) string {
+	// Clean the data
+	content = replaceSpecialChars(content)
+
+	// Remove the content before the PRETTY_NAME field
+	beforeMatch := regexp.MustCompile(".*?PRETTY_NAME=")
+	content = beforeMatch.ReplaceAllString(content, "")
+
+	// Remove the content after the PRETTY_NAME.
+	// The ID field comes right after PRETTY_NAME
+	afterMatch := regexp.MustCompile("ID.*?$")
+	content = afterMatch.ReplaceAllString(content, "")
+
+	return content
+}
+
+// Replace special chars to make the text clean
+func replaceSpecialChars(content string) string {
+	unwantedChar := regexp.MustCompile("\n|\"")
+	content = unwantedChar.ReplaceAllString(content, "")
+	return content
 }
