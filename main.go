5bcd6ebfab76b4206993a7dc2b7e6462b38a1484
Deepjyoti Barman <deep.barman30@gmail.com>
2022-01-19 22:10:42 +0530

    Change Generate content name

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strings"
-	"unicode/utf8"
 
 	"github.com/deepjyoti30/yaf/util"
 )
@@ -19,36 +17,9 @@ func main() {
 		"memory":   util.GetMemory(),
 	}
 
-	rightContent := generateRightContent(details)
+	rightContent := util.GenerateContent(details)
 
 	for _, value := range rightContent {
 		fmt.Println(value)
 	}
-}
-
-// Generate an array of strings to print line by line when
-// fetch is called.
-func generateRightContent(details map[string]string) []string {
-	// First line should be empty
-	var lines = make([]string, 1)
-
-	// Username and hostname
-	lines = append(lines, fmt.Sprint(details["username"], "@", details["hostname"]))
-
-	// Add a separator line
-	lines = append(lines, strings.Repeat("━", utf8.RuneCountInString(lines[1])))
-
-	// Add an empty line
-	lines = append(lines, "")
-
-	// Except username and hostname add rest into proper format
-	for key, value := range details {
-		if key == "username" || key == "hostname" {
-			continue
-		}
-
-		lines = append(lines, util.FormatKeyValue(key, value))
-	}
-
-	return lines
 }
--- util/text.go
+++ util/text.go
@@ -20,3 +20,30 @@ func FormatKeyValue(key string, value string) string {
 	// Add two spaces in between
 	return fmt.Sprint(key, "  ", value)
 }
+
+// Generate an array of strings to print line by line when
+// fetch is called.
+func GenerateContent(details map[string]string) []string {
+	// First line should be empty
+	var lines = make([]string, 1)
+
+	// Username and hostname
+	lines = append(lines, fmt.Sprint(details["username"], "@", details["hostname"]))
+
+	// Add a separator line
+	lines = append(lines, strings.Repeat("━", utf8.RuneCountInString(lines[1])))
+
+	// Add an empty line
+	lines = append(lines, "")
+
+	// Except username and hostname add rest into proper format
+	for key, value := range details {
+		if key == "username" || key == "hostname" {
+			continue
+		}
+
+		lines = append(lines, FormatKeyValue(key, value))
+	}
+
+	return lines
+}
