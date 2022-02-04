d9f71be030270ae1db936b8af0846cf8357fd6ed
Deepjyoti Barman <deep.barman30@gmail.com>
2022-02-04 23:39:27 +0530

    Add init changes to fall back OS specific methods for kernel

--- util/kernel.go
+++ util/kernel.go
@@ -1,9 +1,11 @@
 package util
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"regexp"
+	"runtime"
 )
 
 // Extract the kernel version by reading the /proc/version file
@@ -11,12 +13,22 @@ func GetKernelVersion() string {
 	var procFile = "/proc/version"
 
 	content, err := os.ReadFile(procFile)
-	if err != nil {
-		fmt.Println("couldn't read kernel version")
-		return "not found"
+	if err == nil {
+		return parseProc(string(content))
 	}
 
-	return parseProc(string(content))
+	// Use OS specific methods to extract kernel
+	var kernelVersion = ""
+	var kernelErr error
+	switch runtime.GOOS {
+	case "darwin":
+		kernelVersion, kernelErr = detectDarwinKernel()
+	default:
+		kernelErr = errors.New(fmt.Sprint("no OS specific method to get kernel for: ", runtime.GOOS))
+	}
+
+	fmt.Println("couldn't read kernel version: ", kernelErr)
+	return kernelVersion
 }
 
 func parseProc(content string) string {
@@ -25,4 +37,11 @@ func parseProc(content string) string {
 
 	versionExtracted := re.FindAllString(content, 1)
 	return cleanRe.ReplaceAllString(versionExtracted[0], "")
+}
+
+// detectDarwinKernel detects the kernel version in darwin
+// OS. This is only called if the generic way to extract kernel
+// fails.
+func detectDarwinKernel() (string, error) {
+	return "", nil
 }
